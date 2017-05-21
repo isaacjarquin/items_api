0b8554f1ec827e360a3f815c04e0749298d2f220
isaac jarquin <isaacjarquin@gmail.com>
2017-05-21 22:11:23 +0100

    do some refactoring before deleting images

--- lib/mix/tasks/items.ex
+++ lib/mix/tasks/items.ex
@@ -5,11 +5,18 @@ defmodule Mix.Tasks.Items do
   @shortdoc "remove items no longer available and image from cloudinary"
 
   def remove do
-    {:ok, date} = DateTime.utc_now |> DateTime.to_unix |> (+ 2592000) |> DateTime.from_unix()
+    { :ok, today } = current_date()
 
-    formatted_date = date |> Ecto.Date.cast!
-    query = from i in ItemsApi.Item, where: i.item_removal_date == ^formatted_date
+    current_date_formatted = today |> Ecto.Date.cast!
+    query = from i in ItemsApi.Item, where: i.item_removal_date == ^current_date_formatted
     # query = from i in ItemsApi.Item, where: i.item_removal_date == Date.Ecto.utc
     items = Repo.all(query)
+  end
+
+  def current_date do
+    DateTime.utc_now |>
+    DateTime.to_unix |>
+    (+ 2592000) |>
+    DateTime.from_unix()
   end
 end
